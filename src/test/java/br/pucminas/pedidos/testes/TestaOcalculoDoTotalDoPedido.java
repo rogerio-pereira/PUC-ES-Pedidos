30cc2e87157236d99aaa83ccd3bf713e25923419
Rogério Pereira <rogerio@groupsofter.com.br>
2016-03-28 21:25:08 -0300

    Tiquete #2: Testar o cálculo do total do pedido com 1 item concluido

--- src/test/java/br/pucminas/pedidos/testes/TestaOcalculoDoTotalDoPedido.java
+++ src/test/java/br/pucminas/pedidos/testes/TestaOcalculoDoTotalDoPedido.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import br.pucminas.pedidos.dominio.Pedido;
+import br.pucminas.pedidos.dominio.Pedido;import br.pucminas.pedidos.dominio.Produto;
 
 public class TestaOcalculoDoTotalDoPedido {
 
@@ -12,6 +12,14 @@ public class TestaOcalculoDoTotalDoPedido {
 	public void oTotalDoPedidoDeveraRetornarZeroParaUmPedidoSemItens() {
 		Pedido pedido = new Pedido(0);
 		assertEquals(0.0, pedido.calculaTotal(), 0.01);
+	}
+	
+	@Test
+	public void oTotalDoPedidoDeveraRetornarSomaPedidoComUmItem() {
+		Pedido pedido = new Pedido(1);
+		Produto produto1 =  new Produto(1, "Produto 1", 20.0);
+		pedido.incluiItem(produto1, 2);
+		assertEquals(40.0, pedido.calculaTotal(), 0.01);
 	}
 
 }
