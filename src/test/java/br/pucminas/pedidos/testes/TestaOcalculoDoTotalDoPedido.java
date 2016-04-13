ceda9fda0f23656a198aba3e626fba43571e0e4d
Rogério Pereira <rogerio@groupsofter.com.br>
2016-04-12 21:26:18 -0300

    Refatoração de classe de teste, usando @BeforeClass e @AfterClass

--- src/test/java/br/pucminas/pedidos/testes/TestaOcalculoDoTotalDoPedido.java
+++ src/test/java/br/pucminas/pedidos/testes/TestaOcalculoDoTotalDoPedido.java
@@ -2,6 +2,8 @@ package br.pucminas.pedidos.testes;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import br.pucminas.pedidos.dominio.Produto;
@@ -9,30 +11,50 @@ import br.pucminas.pedidos.dominio.Pedido;
 
 
 public class TestaOcalculoDoTotalDoPedido {
+	private static Produto caneta;
+	private static Produto lapis;
+	
+	@BeforeClass
+	public static void configuraClasseDeTeste()
+	{
+		caneta 	= new Produto(1, "Caneta", 1.50);
+		lapis 	= new Produto(2, "Lapis", 1.00);
+	}
+	
+	
 	@Test
-	public void oTotalDoPedidoDeveraRetornarZeroParaUmPedidoSemItens() {
+	public void oTotalDoPedidoDeveraRetornarZeroParaUmPedidoSemItens() 
+	{
 		Pedido pedido = new Pedido(0);
+		
 		assertEquals(0.0, pedido.calculaTotal(), 0.01);
 	}
 
 	@Test
-	public void oTotalDoPedidoDeveraRetornarUmValorParaUmPedidoCom1Item() {
-		Pedido pedido = new Pedido(0);
-		Produto caneta = new Produto(1, "Caneta", 1.50);
+	public void oTotalDoPedidoDeveraRetornarUmValorParaUmPedidoCom1Item() 
+	{
+		Pedido 	pedido = new Pedido(0);
+		
 		pedido.incluiItem(caneta, 2);
+		
 		assertEquals(3.0, pedido.calculaTotal(), 0.01);
 	}
 
 	@Test
-	public void oTotalDoPedidoDeveraRetornarUmValorParaUmPedidoCom2Itens() {
-
-		Pedido pedido = new Pedido(0);
-		Produto caneta = new Produto(1, "Caneta", 1.50);
-		Produto lapis = new Produto(1, "Lápis", 1.00);
+	public void oTotalDoPedidoDeveraRetornarUmValorParaUmPedidoCom2Itens() 
+	{
+		Pedido 	pedido 	= new Pedido(0);
 
 		pedido.incluiItem(caneta, 2);
 		pedido.incluiItem(lapis, 3);
 
 		assertEquals(6.0, pedido.calculaTotal(), 0.01);
+	}
+	
+	@AfterClass
+	public static void desconfiguraClasseDeTeste()
+	{
+		caneta 	= null;
+		lapis 	= null;
 	}
 }
