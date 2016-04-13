81a8f6f70da17916d2ea306f751e324f60fe6824
Rogério Pereira <rogerio@groupsofter.com.br>
2016-04-12 21:16:35 -0300

    Correção de codigo para deixar igual a aula

--- src/test/java/br/pucminas/pedidos/testes/TestaOcalculoDoTotalDoPedido.java
+++ src/test/java/br/pucminas/pedidos/testes/TestaOcalculoDoTotalDoPedido.java
@@ -1,37 +1,38 @@
 package br.pucminas.pedidos.testes;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import br.pucminas.pedidos.dominio.Pedido;import br.pucminas.pedidos.dominio.Produto;
+import br.pucminas.pedidos.dominio.Produto;
+import br.pucminas.pedidos.dominio.Pedido;
+
 
 public class TestaOcalculoDoTotalDoPedido {
-
 	@Test
 	public void oTotalDoPedidoDeveraRetornarZeroParaUmPedidoSemItens() {
 		Pedido pedido = new Pedido(0);
 		assertEquals(0.0, pedido.calculaTotal(), 0.01);
 	}
-	
-	@Test
-	public void oTotalDoPedidoDeveraRetornarSomaPedidoComUmItem() {
-		Pedido pedido = new Pedido(1);
-		Produto produto1 =  new Produto(1, "Produto 1", 20.0);
-		pedido.incluiItem(produto1, 2);
-		assertEquals(40.0, pedido.calculaTotal(), 0.01);
-	}
-	
 
-	
 	@Test
-	public void oTotalDoPedidoDeveraRetornarSomaPedidoComDoisItens() {
-		Pedido pedido = new Pedido(1);
-		Produto produto1 =  new Produto(1, "Produto 1", 20.0);
-		Produto produto2 =  new Produto(2, "Produto 2", 10.0);
-		pedido.incluiItem(produto1, 2);
-		pedido.incluiItem(produto2, 1);
-		assertEquals(50.0, pedido.calculaTotal(), 0.01);
+	public void oTotalDoPedidoDeveraRetornarUmValorParaUmPedidoCom1Item() {
+		Pedido pedido = new Pedido(0);
+		Produto caneta = new Produto(1, "Caneta", 1.50);
+		pedido.incluiItem(caneta, 2);
+		assertEquals(3.0, pedido.calculaTotal(), 0.01);
 	}
 
+	@Test
+	public void oTotalDoPedidoDeveraRetornarUmValorParaUmPedidoCom2Itens() {
+
+		Pedido pedido = new Pedido(0);
+		Produto caneta = new Produto(1, "Caneta", 1.50);
+		Produto lapis = new Produto(1, "Lápis", 1.00);
+
+		pedido.incluiItem(caneta, 2);
+		pedido.incluiItem(lapis, 3);
+
+		assertEquals(6.0, pedido.calculaTotal(), 0.01);
+	}
 }
